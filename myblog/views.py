ce3a0c1f57e1037a25c8ce338aed96c08033b0fe
YuuhiSakamoto <neipia_number@icloud.com>
2020-11-16 22:26:26 +0900

    myblog_index 変数を利用

--- myblog/views.py
+++ myblog/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-  return render(request, 'myblog/index.html')
+  params = {
+    'title': 'myblog',
+    'msg': '初の自分のブログです'
+  }
+  return render(request, 'myblog/index.html', params)
