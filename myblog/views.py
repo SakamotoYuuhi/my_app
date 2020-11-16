199f187e46fb322f0f2d48d056669fda8120c9b4
YuuhiSakamoto <neipia_number@icloud.com>
2020-11-16 23:39:32 +0900

    新規作成ページとリンク作成、Bootstrap4の追加

--- myblog/views.py
+++ myblog/views.py
@@ -4,6 +4,15 @@ from django.http import HttpResponse
 def index(request):
   params = {
     'title': 'myblog',
-    'msg': '初の自分のブログです'
+    'msg': '初の自分のブログです',
+    'goto': 'new',
   }
   return render(request, 'myblog/index.html', params)
+
+def new(request):
+  params = {
+    'title': 'new topic',
+    'msg': '新しい記事を作成できます',
+    'goto': 'index',
+  }
+  return render(request, 'myblog/new.html', params)
