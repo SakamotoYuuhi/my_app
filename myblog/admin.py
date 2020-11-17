b9d2ad23d323e70ba8a26dbb025a1ec090345bab
YuuhiSakamoto <neipia_number@icloud.com>
2020-11-17 15:55:53 +0900

    データベースの作成と初期設定

--- myblog/admin.py
+++ myblog/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import MyBlogModel
 
-# Register your models here.
+admin.site.register(MyBlogModel)
--- myblog/models.py
+++ myblog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class MyBlogModel(models.Model):
+  topic_title = models.CharField(max_length=50)
+  content = models.CharField(max_length=200)
+
+  def __str__(self):
+    return '<Friend:id=' + str(self.id) + ', タイトル名:' + \
+            self.topic_title + '>'
