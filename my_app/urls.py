17084465ac8d84e3bad87ff4435e44152504a9b1
YuuhiSakamoto <neipia_number@icloud.com>
2021-01-09 00:08:43 +0900

    ポートフォリオの作成とWebページの表示

--- my_app/urls.py
+++ my_app/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('myblog/', include('myblog.urls')),
+    path('portfolio/', include('portfolio.urls')),
 ]
 # 画像アップロード用にsettings.pyにパスを通すために追記
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
