98fbf414bc4603d51f8c34eb1ced6d546ad84383
YuuhiSakamoto <neipia_number@icloud.com>
2021-01-13 06:23:49 +0900

    連絡用フォームの作成

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,15 +1,7 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
-# from .views import IndexView, CreateView, DetailView, EditView, DeleteView
-from . import views
+from .views import ContactFormView, ContactResultView
 
 urlpatterns = [
-  path('', views.index, name='index'),
-  # path('<int:page>', IndexView.as_view(), name='index'),
-  # path('detail/<int:pk>', DetailView.as_view(), name='detail'),
-  # path('create', CreateView.as_view(), name='create'),
-  # path('edit/<int:pk>', EditView.as_view(), name='edit'),
-  # path('delete/<int:pk>', DeleteView.as_view(), name='delete'),
-  # path('login', LoginView.as_view(template_name='myblog/login.html'), name='login'),
-  # path('logout', LogoutView.as_view(template_name='myblog/logout.html'), name='logout'),
+  path('', ContactFormView.as_view(), name='index'),
+  path('contact_result', ContactResultView.as_view(), name='contact_result'),
 ]
